bc50e8c7a7a85c4f9a181c460e8a0fad77e67abe
Aman <aalix1803@gmail.com>
2025-07-24 19:50:37 +0530

    login , signup and navbar  page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
+import Login from './components/auth/login'
+import Signup from './components/auth/signup'
+import Home from './components/Home'
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+    path:'/signup',
+    element:<Signup/>
+  },
+  {
+    path:'/',
+    element:<Home/>
+  },
+])
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className='text-red-500'>Lets build JOB OP </h1>
+      <RouterProvider router={appRouter}/>
     </>
   )
 }
