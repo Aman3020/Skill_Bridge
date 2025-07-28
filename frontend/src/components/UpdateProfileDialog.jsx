6afa72598e41188e3fbfcd9b0b50b2c524870650
Aman <aalix1803@gmail.com>
2025-07-29 01:45:07 +0530

    updateprofile componenet

--- frontend/src/components/UpdateProfileDialog.jsx
+++ frontend/src/components/UpdateProfileDialog.jsx
@@ -1,11 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Loader } from "lucide-react";
+import { useSelector } from "react-redux";
 
-const UpdateProfileDialog = () => {
+const UpdateProfileDialog = ({ open, setOpen }) => {
+  const [loading, setLoading] = useState(false);
+  const {user} = useSelector(store=>store.auth);
+
+  const [input, setInput] = useState({
+    fullname:user?.fullname,
+    email:user?.email,
+    phoneNumber:user?.phoneNumber,
+    bio:user?.profile?.bio,
+    skills:user?.profile?.skills?.map(skill => skill),
+    file:user?.profile?.resume
+  })
   return (
     <div>
-        
+      <Dialog open={open}>
+        <DialogContent
+          className="sm:max-w-[425px]"
+          onInteractOutside={() => setOpen(false)}
+        >
+          <DialogHeader>
+            <DialogTitle>Update Profile</DialogTitle>
+          </DialogHeader>
+          <form>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" value={input.fullname} name="name" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" value={input.email} name="email" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="number">Number</Label>
+                <Input id="number" value={input.phoneNumber} name="number" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="bio">Bio</Label>
+                <Input id="bio" name="bio" value={input.bio} className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="skills">Skills</Label>
+                <Input id="skills" value={input.skills} name="skills" className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="file">Resume</Label>
+                <Input
+                  id="file"
+                  name="file"
+                  type="file"
+                  accept="application/pdf"
+                  className="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              {loading ? (
+                <Button className="w-full my-4">
+                  <Loader className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </Button>
+              ) : (
+                <Button type="submit" className="w-full my-4">
+                  Update
+                </Button>
+              )}
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
-}
+  );
+};
 
-export default UpdateProfileDialog
+export default UpdateProfileDialog;
