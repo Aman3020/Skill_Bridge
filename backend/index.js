907a6656b9223b10ad161cf0e191c6973f329048
CodaHolic <aman03singh18@gmail.com>
2025-09-02 11:17:16 +0530

    added the backend server uri

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://skill-bridge-3q7z.onrender.com',
     credentials:true
 }
 
